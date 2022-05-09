678a49be6150b708d1ec38bac0dfbca02ff585ee
PatrykRomanowski <71602577+PatrykRomanowski@users.noreply.github.com>
2022-05-09 21:19:59 +0200

    adding collecting key

---
+++ main.js
@@ -0,0 +1,20 @@
+// import { Game } from 'phaser'
+// import { config } from './preload'
+
+
+// var game = new Game(config); //nie dokońca wiem po co to jest, do ustalenia
+    
+    
+
+// export default class MainScene extends Phaser.Scene
+// {
+//     preload()
+//     {
+
+//     }
+
+//     create()
+//     {
+
+//     }
+// }
